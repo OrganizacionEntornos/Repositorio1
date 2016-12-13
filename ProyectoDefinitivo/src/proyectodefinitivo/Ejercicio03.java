71e19bccdead44509fb039dd504daf806f6bfe7e
1DAM JC <1DAM@CFA3-P19>
2016-12-13 17:32:13 +0100

    PRIMERA VERSIÓN DEL EJERCICIO 03

--- ProyectoDefinitivo/src/proyectodefinitivo/Ejercicio03.java
+++ ProyectoDefinitivo/src/proyectodefinitivo/Ejercicio03.java
@@ -1,14 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package proyectodefinitivo;
-
-/**
- *
- * @author 1DAM
+import java.util.Scanner;
+/**EJERCICIO3 v1.0
+ * 
+ * Programa que calcula el cuadrado y el cubo de un número introducido por el usuario.
+ * 
+ * @author JC Romero
  */
-public class Ejercicio03 {
-    
+public class Ejercicio03
+{
+
+    public static void main(String[] args)
+    {        
+        Scanner userInput = new Scanner(System.in);
+        int number;
+        
+        System.out.println("Por favor, introduzca un número entero:");
+        number = userInput.nextInt();
+        
+        System.out.println("El cuadrado de " + number + " es: " + number*number);
+        System.out.println("El cubo de " + number + " es: " + number*number*number);
+    }
 }
