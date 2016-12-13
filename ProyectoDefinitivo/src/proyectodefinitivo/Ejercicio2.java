6e80cc44865becdfa2062ce33440b055b9190310
1DAM JC <1DAM@CFA3-P19>
2016-12-13 19:17:44 +0100

    1ª MODIFICACIÓN EJERCICIO 2
    Añadido un método Main...

--- ProyectoDefinitivo/src/proyectodefinitivo/Ejercicio2.java
+++ ProyectoDefinitivo/src/proyectodefinitivo/Ejercicio2.java
@@ -1,14 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package proyectodefinitivo;
-
 /**
- * @author Expression SergioBatuecas is undefined on line 11, column 14 in Templates/Classes/Class.java.
+ * @author SergioBatuecas
+ * 
+ * Añadida clase y método para calcular el cambio de una cantidad de dinero dada por el usuario.
  */
-public class Ejercicio2 {
 
+import java.util.Scanner;
+public class Ejercicio2 
+{
+    public static void main(String[] args) 
+    {
+        int money;
+        int of50, of20, of10, of5, of2, of1;
+        
+        Scanner userInput = new Scanner (System.in);
+        
+        System.out.println("Por favor, indique una cantidad de dinero");
+        money = userInput.nextInt();
+        of50 = money/50;
+        of20 = (money%50)/20;
+        of10 = ((money%50)%20)/10;
+        of5 = (((money%50)%20)%10)/5;
+        of2 = ((((money%50)%20)%10)%5)/2;
+        of1 = ((((money%50)%20)%10)%5)%2;
+        System.out.println(money + "€ se descomponen en " + of50 + " billete(s) de 50, " +
+                of20 + " billete(s) de 20, " + of10 + " billete(s) de 10, " + of5 + " billete(s) de 5, "+
+                of2 + " moneda(s) de 2 euros y " + of1 + " moneda(s) de 1 euro.");
+    }
 }
