e43a7b72131ef317c7b3f64a5700f02a1f25d35e
1DAM <1DAM@CFA3-P19>
2016-12-14 20:46:16 +0100

    Ejercicio 03 - añadida calculadora de tiempo

--- ProyectoDefinitivo/src/proyectodefinitivo/Ejercicio03.java
+++ ProyectoDefinitivo/src/proyectodefinitivo/Ejercicio03.java
@@ -1,13 +1,26 @@
-
 package proyectodefinitivo;
-
+import java.util.Scanner;
 /**
  *
  * @author JCRomero
  */
 public class Ejercicio03
 {
-    int number1 = 0;
-    int number2 = 1;
-    
+    public static void calculateTime(String[] args)
+    {
+        int time;
+        int days, hours, minutes, seconds; 
+        
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Por favor, introduzca un número de segundos: ");
+        time = userInput.nextInt();
+        
+        days = time / (60*60*24);
+        hours = (time % (60*60*24))/(60*60);
+        minutes = ((time % (60*60*24))%(60*60))/60;
+        seconds = ((time % (60*60*24))%(60*60))%60;
+        
+        System.out.println (time + " segundos hacen un total de: " + days + " días, "+ 
+                hours + " horas, " + minutes + " minutos y " + seconds + " segundos.");
+    }
 }
